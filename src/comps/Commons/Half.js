b3498bc6ddf8d1c0a2b2eb0715a8c9fb7265012c
D <htehran333@gmail.com>
2024-05-02 09:54:17 -0700

    Services Done

--- src/comps/Commons/Half.js
+++ src/comps/Commons/Half.js
@@ -1,21 +1,24 @@
-import React from "react";
+import { Button } from "../Portal";
 
 const Half = ({ thing, reverse = false }) => {
   return (
     <section
-      className={`padding-x py-10 flex md:flex-row ${
-        reverse ? "flex-col-reverse" : "flex-col"
+      className={`padding-x py-10 flex ${
+        reverse
+          ? "md:flex-row-reverse flex-col"
+          : "md:flex-row flex-col-reverse"
       }`}
     >
       <div className="md:w-1/2 flex-fullcenter">
         <div dir="rtl">
           <h1 className="title font-bold">{thing.title}</h1>
-          <p className="max-md:text-sm mt-5">{thing.describe}</p>
+          <p className="max-md:text-sm my-5 text-justify">{thing.describe}</p>
+          <Button to="/" label="شروع همکاری" />
         </div>
       </div>
       <div className="md:w-1/2">
-        <div className="padding">
-          {thing.cover.endsWith(".mp4") || thing.cover.endsWith(".webm") ? (
+        <div className="padding flex-fullcenter">
+          {thing.cover?.endsWith(".mp4") || thing.cover?.endsWith(".webm") ? (
             <video autoPlay muted loop className="bg-fullobject w-full">
               <source
                 src={thing.cover}
@@ -23,6 +26,8 @@ const Half = ({ thing, reverse = false }) => {
               />
               Your browser does not support the video tag.
             </video>
+          ) : thing.icon ? (
+            <i className={thing.icon + " md:text-[300px] text-[100px]"} />
           ) : (
             <img
               src={thing.cover}
--- src/comps/Home/Services.js
+++ src/comps/Home/Services.js
@@ -1,21 +1,42 @@
-import React from "react";
 import { homeServices } from "../../Constants";
 import { Link } from "react-router-dom";
 
 const Services = () => {
   return (
     <section className="padding-x py-10">
-      <h2 className="text-center title">خدمات ما</h2>
+      <h2 className="text-center title-lg">خدمات ما</h2>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-5 mt-12" dir="rtl">
         {homeServices.map((item, index) => {
           return (
             <div className="flex-fullcenter">
               <Link
                 to="/"
-                className="size-full border border-slate-100 p-5 text-center transition-colors hover:bg-sky-500"
+                className="size-full bg-blue-600 border-4 border-slate-100 transition-all duration-300 rounded-3xl backdrop-blur hover:scale-105 flex flex-col justify-between overflow-hidden group"
               >
-                <h3 className="text-xl">{item.title}</h3>
-                <p className="text-sm">{item.describe}</p>
+                <div className={`p-5 relative transition-all`}>
+                  <div className="md:text-3xl md:my-5 flex items-center transition-all md:group-hover:text-xl md:group-hover:my-0">
+                    <i className={item.icon} />
+                    <h3 className="ps-2">{item.title}</h3>
+                  </div>
+                  <p className="text-sm mt-3 displayTrans md:opacity-0 md:invisible md:absolute md:group-hover:opacity-100 md:group-hover:visible">
+                    {item.describe}
+                  </p>
+                </div>
+                <Link
+                  to="#"
+                  class="relative border-t-4 border-slate-100 inline-flex items-center justify-start py-3 pl-4 pr-12 font-bold text-blue-600 transition-all hover:pl-10 hover:pr-6 bg-slate-100 group/lets"
+                >
+                  <span class="absolute bottom-0 left-0 w-full h-0 transition-all duration-150 ease-in-out bg-blue-600 group-hover/lets:h-full"></span>
+                  <span class="absolute text-slate-950 right-0 pr-4 duration-200 ease-out group-hover/lets:translate-x-12">
+                    <i className="fal fa-chevron-right" />
+                  </span>
+                  <span class="absolute text-slate-50 left-0 pl-2.5 -translate-x-12 group-hover/lets:translate-x-0 ease-out duration-200">
+                    <i className="fal fa-chevron-right" />
+                  </span>
+                  <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover/lets:text-white">
+                    بزن بریم
+                  </span>
+                </Link>
               </Link>
             </div>
           );
